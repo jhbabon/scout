073e5ef2a75cecced92e39451624fe12e95e0ce9
Juan Hernández <juan.hernandez.babon@gmail.com>
2017-03-08 20:08:56 +0100

    Dirty experiment with /dev/tty
    You can input to the program, show it into the TTY and print out what
    you write in the tty.
    
    $ echo foo | cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{fs, io};
+use std::io::{Read, Write};
+
+fn magic() -> Result<(), io::Error> {
+    println!("The magic begins");
+
+    let mut input = String::new();
+    try!(io::stdin().read_to_string(&mut input));
+
+    let mut tty = try!(fs::OpenOptions::new().read(true).write(true).open("/dev/tty"));
+
+    let _ = writeln!(&mut tty, "{}", input);
+
+    let mut buffer = String::new();
+    match tty.read_to_string(&mut buffer) {
+        Ok(_) => println!("Input: {}", buffer),
+        Err(e) => println!("Error: {}", e),
+    };
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = magic();
 }
