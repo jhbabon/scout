6d340e9963ec6f1f2b35ca4dc1a3c586432710b1
Juan Hernández <juan.hernandez.babon@gmail.com>
2017-04-14 21:04:45 +0200

    Experiments with /dev/tty

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ use std::io::{Read, Write};
 fn magic() -> Result<(), io::Error> {
     println!("The magic begins");
 
-    let mut input = String::new();
-    try!(io::stdin().read_to_string(&mut input));
+    // let mut input = String::new();
+    // try!(io::stdin().read_to_string(&mut input));
 
     let mut tty = try!(fs::OpenOptions::new().read(true).write(true).open("/dev/tty"));
 
-    let _ = writeln!(&mut tty, "{}", input);
+    // let _ = writeln!(&mut tty, "{}", input);
+    let mut counter = 0;
 
-    let mut buffer = String::new();
-    match tty.read_to_string(&mut buffer) {
-        Ok(_) => println!("Input: {}", buffer),
-        Err(e) => println!("Error: {}", e),
+    'event: loop {
+        let mut buffer = String::new();
+        try!(tty.read_to_string(&mut buffer));
+        let _ = writeln!(&mut tty, "Your input: {}", buffer);
+        counter += 1;
+
+        if counter > 3 {
+            break 'event;
+        }
     };
 
     Ok(())
