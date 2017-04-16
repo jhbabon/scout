013c0f18f1b62681b5cc9bde9aca9cfe7f6fa031
Juan Hernández <juan.hernandez.babon@gmail.com>
2017-04-16 13:48:05 +0200

    Escape special regex chars from user input.
    If not, the program crashes.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,22 @@ extern crate regex;
 
 use regex::Regex;
 
+/// Get the version of the program.
+pub fn version() -> String {
+    let (maj, min, pat) = (option_env!("CARGO_PKG_VERSION_MAJOR"),
+                           option_env!("CARGO_PKG_VERSION_MINOR"),
+                           option_env!("CARGO_PKG_VERSION_PATCH"));
+
+    match (maj, min, pat) {
+        (Some(maj), Some(min), Some(pat)) => format!("{}.{}.{}", maj, min, pat),
+        _ => "".to_string(),
+    }
+}
+
 // Idea taken from:
 //   http://blog.amjith.com/fuzzyfinder-in-10-lines-of-python
 pub fn explore<'a>(list: &'a [&'a str], query: &'a [char]) -> Vec<&'a str> {
-    let pattern: String = query.iter()
-                        .map(|ch| format!("(?i){}.*?", ch)) // (?i) for case insensitive
-                        .collect();
+    let pattern = build_pattern(query);
     let re = Regex::new(&pattern).unwrap();
 
     let mut suggestions: Vec<(usize, usize, &str)> = list.iter()
@@ -21,27 +31,25 @@ pub fn explore<'a>(list: &'a [&'a str], query: &'a [char]) -> Vec<&'a str> {
     suggestions.iter().map(|&(_, _, item)| item).collect()
 }
 
-/// Get the version of the program.
-pub fn version() -> String {
-    let (maj, min, pat) = (option_env!("CARGO_PKG_VERSION_MAJOR"),
-                           option_env!("CARGO_PKG_VERSION_MINOR"),
-                           option_env!("CARGO_PKG_VERSION_PATCH"));
-
-    match (maj, min, pat) {
-        (Some(maj), Some(min), Some(pat)) => format!("{}.{}.{}", maj, min, pat),
-        _ => "".to_string(),
-    }
+fn build_pattern<'a>(query: &'a [char]) -> String {
+    query.iter()
+        .map(|ch| regex::escape(&ch.to_string()))
+        .map(|esc| format!("(?i){}.*?", esc)) // (?i) for case insensitive
+        .collect()
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const LIST: [&'static str; 4] = [
+    const LIST: [&'static str; 6] = [
         "/some/deeper/path/users.rs",
         "/some/path/api_user.rs",
         "/some/path/user_group.rs",
         "foobar.rs",
+        "reserved?*.rs",
+        "ßℝ💣",
     ];
 
     #[test]
@@ -63,6 +71,26 @@ mod tests {
             "/some/path/user_group.rs",
             "/some/path/api_user.rs",
             "/some/deeper/path/users.rs",
+        ];
+
+        assert_eq!(expected, explore(&LIST, &query));
+    }
+
+    #[test]
+    fn it_takes_reserved_chars() {
+        let query = ['?', '*', '.'];
+        let expected = vec![
+            "reserved?*.rs"
+        ];
+
+        assert_eq!(expected, explore(&LIST, &query));
+    }
+
+    #[test]
+    fn it_takes_special_chars() {
+        let query = ['ß', '💣'];
+        let expected = vec![
+            "ßℝ💣"
         ];
 
         assert_eq!(expected, explore(&LIST, &query));
