0489d8e46b7fe8c7ffd15b5d9d4cb64888ab59ac
Juan Hernández <juan.hernandez.babon@gmail.com>
2017-07-03 13:12:32 +0200

    Use the original error messages in the new Error type

--- src/scout/errors.rs
+++ src/scout/errors.rs
@@ -1,35 +1,52 @@
 use std::{fmt, error, io};
-
 use regex;
 
-pub struct Error;
+pub struct Error {
+    display: String,
+    debug: String,
+    description: String,
+}
+
+impl Error {
+    pub fn import<E: fmt::Display + fmt::Debug + error::Error>(error: E) -> Self {
+        let display = format!("{}", error);
+        let debug = format!("{:?}", error);
+        let description = error.description().to_owned();
+
+        Error {
+            display,
+            debug,
+            description,
+        }
+    }
+}
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("Internal error")
+        f.write_str(&self.display)
     }
 }
 
 impl fmt::Debug for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str("Internal error")
+        f.write_str(&self.debug)
     }
 }
 
 impl error::Error for Error {
     fn description(&self) -> &str {
-        "Something happened!"
+        &self.description
     }
 }
 
 impl From<regex::Error> for Error {
-    fn from(_error: regex::Error) -> Self {
-        Error
+    fn from(error: regex::Error) -> Self {
+        Error::import(error)
     }
 }
 
 impl From<io::Error> for Error {
-    fn from(_error: io::Error) -> Self {
-        Error
+    fn from(error: io::Error) -> Self {
+        Error::import(error)
     }
 }
