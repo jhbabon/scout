f69d8db7e35a49907092f8d32ac954041a981c7f
Juan Hernández <juan.hernandez.babon@gmail.com>
2019-10-13 15:00:52 +0100

    Try to parse events from PTTY input as bytes come in

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![recursion_limit="256"]
-// use futures::{select, FutureExt};
+
 use async_std::io;
-use async_std::os::unix::io::AsRawFd;
 use async_std::prelude::*;
 use async_std::task;
 use async_std::fs;
+use async_std::os::unix::io::AsRawFd;
+
 use termios::{self, Termios};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+enum Packet {
+    Inbound(String),
+    Byte(u8),
+}
 
 async fn get_tty() -> Result<fs::File> {
     let tty = fs::OpenOptions::new()
@@ -48,15 +54,77 @@ fn main() -> Result<()> {
         let std_reader = io::BufReader::new(stdin);
         let tty_reader = io::BufReader::new(tty_in);
 
-        let std_lines = std_reader.lines();
-        let tty_lines = tty_reader.lines();
+        let std_lines = std_reader.lines().map(|l| {
+            if let Ok(line) = l {
+                Some(Packet::Inbound(line))
+            } else {
+                None
+            }
+        });
 
+        // FIXME: Probably is better if the tty stream does the byte to chars conversion
+        // let tty_lines = tty_reader.bytes().scan(Vec::new(), |state, b| {
+        //     if let Ok(byte) = b {
+        //         state.push(byte);
+        //         Some(Some(Packet::Byte(byte)))
+        //     } else {
+        //         None
+        //     }
+        // });
+
+        let tty_lines = tty_reader.bytes().map(|b| {
+            if let Ok(byte) = b {
+                Some(Packet::Byte(byte))
+            } else {
+                None
+            }
+        });
+
+        // This select works in a round robin fashion
         let mut all = futures::stream::select(tty_lines, std_lines);
 
-        while let Some(line) = all.next().await {
-            let l = format!("{}\n", line?);
-            tty_out.write_all(l.as_bytes()).await?;
-            tty_out.flush().await?;
+        let mut buf = Vec::with_capacity(10);
+
+        while let Some(packet) = all.next().await {
+            let line = match packet {
+                Some(Packet::Inbound(s)) => {
+                    let l = format!("STDIN: {}", s);
+                    Some(l)
+                },
+                Some(Packet::Byte(byte)) => {
+                    // FIXME: some keys have more than one byte, i.e: Up, Down, etc
+                    //   these start with escape sequences, the char: \u{1b}
+                    //   In order to intepret them correctly the progam has to
+                    //   wait until the next squence of bytes based on the first one
+                    buf.push(byte);
+                    let chars = match String::from_utf8(buf.clone()) {
+                        Ok(s) => {
+                            buf.clear();
+                            Some(s)
+                        },
+                        Err(_) => {
+                            // probably the program needs the next byte to make sense of it
+                            None
+                        }
+                    };
+
+                    if let Some(chrs) = chars {
+                        let ch = chrs.chars()
+                            .fold("".to_string(), |acc, key| format!("{}{:?}", acc, key));
+                        Some(ch)
+                    } else {
+                        None
+                    }
+
+                },
+                None => None,
+            };
+
+            if let Some(l) = line {
+                let l = format!("{}\n", l);
+                tty_out.write_all(l.as_bytes()).await?;
+                tty_out.flush().await?;
+            }
         }
 
         Ok(())
