dafc92018243d308f8cf3bcb7e8c01e99e5ebefa
Juan Hernández <juan.hernandez.babon@gmail.com>
2019-10-15 22:19:12 +0100

    Be able to use termion to detect input keys
    Use a new Stream that reads the async tty and uses the buffer
    to get the input keys thanks to termion crate

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ extern crate log;
 
 use rayon::prelude::*;
 
+use std::pin::Pin;
+use std::collections::VecDeque;
+
+use async_std::prelude::*;
 use async_std::future::join;
 use async_std::io;
-use async_std::prelude::*;
 use async_std::task;
+use async_std::task::{Context, Poll};
 use async_std::fs;
 use async_std::os::unix::io::AsRawFd;
 
 use futures::{channel, SinkExt};
 
 use termios::{self, Termios};
+use termion::input::TermRead;
+use termion::event::Key;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
@@ -22,9 +28,9 @@ type Receiver<T> = channel::mpsc::UnboundedReceiver<T>;
 #[derive(Debug)]
 enum Packet {
     Inbound(String),
-    Char(char), // TODO: Create Key enum
-    Ignore,
+    // Ignore,
     Done,
+    Key(Key),
 }
 
 #[derive(Debug,Clone, Default)]
@@ -60,6 +66,77 @@ impl State {
     }
 }
 
+fn inbound(
+    r: impl io::Read + Unpin,
+) -> impl Stream<Item = Packet> + Unpin {
+    struct Inbound<R> {
+        reader: R,
+        buffer: VecDeque<Key>,
+    };
+
+    impl<R: io::Read + Unpin> Inbound<R> {
+        fn new(r: R) -> Self {
+            Self {
+                reader: r,
+                buffer: VecDeque::new(),
+            }
+        }
+    }
+
+    impl<R: io::Read + Unpin> Stream for Inbound<R> {
+        type Item = Packet;
+
+        fn poll_next(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+        ) -> Poll<Option<Self::Item>> {
+
+            if let Some(k) = self.buffer.pop_front() {
+                return Poll::Ready(Some(Packet::Key(k)));
+            }
+
+            let mut buf = vec![0; 4];
+            let mut fut = self.reader.read(&mut buf);
+
+            match Pin::new(&mut fut).poll(cx) {
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(Ok(n)) => {
+                    debug!("[inbound.poll_next()] bytes read: {:?}", n);
+
+                    let tmp: Vec<u8> = buf
+                        .iter()
+                        .take(n)
+                        .cloned()
+                        .collect();
+
+                    let mut keys = tmp
+                        .keys()
+                        .filter(|k| k.is_ok())
+                        .map(|k| k.unwrap());
+
+                    let key = keys.next();
+                    debug!("[inbound.poll_next()] key: {:?}", key);
+
+                    while let Some(k) = keys.next() {
+                        match k {
+                            Key::Null => (),
+                            _ => self.buffer.push_back(k),
+                        }
+                    }
+                    debug!("[inbound.poll_next()] extra keys: {:?}", self.buffer);
+
+                    Poll::Ready(key.map(|k| Packet::Key(k)))
+                },
+                Poll::Ready(Err(_)) => {
+                    Poll::Ready(None)
+                },
+            }
+        }
+    }
+
+    Inbound::new(r)
+}
+
 async fn input_loop(mut wire: Sender<Packet>) -> Result<()> {
     debug!("[input_loop] start");
 
@@ -80,7 +157,7 @@ async fn input_loop(mut wire: Sender<Packet>) -> Result<()> {
     // END:   RAW MODE
 
     let std_reader = io::BufReader::new(stdin);
-    let tty_reader = io::BufReader::new(tty_in);
+    // let tty_reader = io::BufReader::new(tty_in);
 
     let std_lines = std_reader.lines()
         .map(|res| {
@@ -90,41 +167,16 @@ async fn input_loop(mut wire: Sender<Packet>) -> Result<()> {
         });
         // .chain(stream::once(Packet::Done));
 
-    // TODO: Transform sequence of bytes into Keys (arrow keys, Ctrl, chars, etc)
-    // TODO: Probably is a good idea to add some debouncing
-    let tty_chars = tty_reader.bytes()
-        .map(|res| res.expect("Error reading from PTTY"))
-        .scan(Vec::new(), |state, byte| {
-            state.push(byte);
-
-            let packet = match String::from_utf8(state.clone()) {
-                Ok(s) => {
-                    state.clear();
-                    if let Some(ch) = s.chars().next() {
-                        Some(Packet::Char(ch))
-                    } else {
-                        Some(Packet::Ignore)
-                    }
-                },
-                Err(_) => {
-                    // probably the program needs the next byte to make sense of it
-                    Some(Packet::Ignore)
-                }
-            };
-
-            packet
-        });
+    let tty_chars = inbound(tty_in);
 
     // This select works in a round robin fashion
     let mut all = futures::stream::select(tty_chars, std_lines);
 
     while let Some(packet) = all.next().await {
         match packet {
-            // Shutting down from here works!
-            // I think the problem was the back and forth between the broker
-            // task and this one with the shutdown channel
-            // I'll try to keep the direction of comms one way only
-            Packet::Char('\n') => {
+            Packet::Key(Key::Char('\n')) |
+            Packet::Key(Key::Alt('\u{0}')) |
+            Packet::Key(Key::Esc) => {
                 wire.send(Packet::Done).await?;
                 break
             },
@@ -164,13 +216,18 @@ async fn broker_loop(mut packets: Receiver<Packet>) -> Result<()> {
             Packet::Inbound(s) => {
                 state.add_string(s);
             },
-            Packet::Char(ch) => {
-                state.add_char(ch);
-                debug!("[broker_loop] start fuzzy search");
-                state.search();
-                debug!("[broker_loop] end fuzzy search");
+            Packet::Key(k) => {
+                match k {
+                    Key::Char(ch) => {
+                        state.add_char(ch);
+                        debug!("[broker_loop] start fuzzy search");
+                        state.search();
+                        debug!("[broker_loop] end fuzzy search");
+                    },
+                    _ => (),
+                };
             },
-            Packet::Ignore => (),
+            // Packet::Ignore => (),
             Packet::Done => break,
         };
 
